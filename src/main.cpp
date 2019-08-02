632e25cb58885003c08a13d71df33c0c98efbebd
Ondrej Dolejsi <ondrej.dolejsi@gmail.com>
2019-08-02 01:19:06 -0400

    First Commit - Blink

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by fuho on 8/2/19.
+//
+
+#include <Arduino.h>
+
+#ifndef LED_BUILTIN
+  #define LED_BUILTIN PC13
+#endif
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(100);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+   // wait for a second
+  delay(100);
+}
