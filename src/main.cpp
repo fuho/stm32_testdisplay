10a44184152a16e3fc4f791ab9ee988da802c435
Ondrej Dolejsi <ondrej.dolejsi@gmail.com>
2019-08-05 22:37:33 -0400

    Growing squares

--- src/main.cpp
+++ src/main.cpp
@@ -56,52 +56,53 @@ Last modification: 10/05/2019
 
 Adafruit_SSD1306 display(128, 64);  // Create display
 
-int Variable1;  // Create a variable to have something dynamic to show on the display
+int number;
 
-void setup()  // Start of setup
-{
-
-    delay(100);  // This delay is needed to let the display to initialize
-
+void setup() {
+    delay(50);
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // Initialize display with the I2C address of 0x3C
-
     display.clearDisplay();  // Clear the buffer
-
     display.setTextColor(WHITE);  // Set color of the text
-
     display.setRotation(0);  // Set orientation. Goes from 0, 1, 2 or 3
-
     display.setTextWrap(false);  // By default, long lines of text are set to automatically “wrap” back to the leftmost column.
     // To override this behavior (so text will run off the right side of the display - useful for
     // scrolling marquee effects), use setTextWrap(false). The normal wrapping behavior is restored
     // with setTextWrap(true).
-
     display.dim(0);  //Set brightness (0 is maximun and 1 is a little dim)
+}
 
-}  // End of setup
+void loop() {
+    for (int x = 0; x < 65; x++) {
+        display.fillRect(0, 0, x, x, WHITE);
+        display.fillRect(128 - x, 64 - x, x, x, WHITE);
+        display.display();
+    }
+    for (int x = 0; x < 65; x++) {
+//        display.clearDisplay();
+        display.fillRect(0, 0, x, x, BLACK);
+        display.fillRect(128 - x, 64 - x, x, x, BLACK);
+        display.display();
+    }
+    display.clearDisplay();
+}
 
 
+/*
+void loop(){
+    number++;  // Increase variable by 1
 
-
-
-
-void loop()  // Start of loop
-{
-
-    Variable1++;  // Increase variable by 1
-    if(Variable1 > 150)  // If Variable1 is greater than 150
+    if(number > 150)  // If number is greater than 150
     {
-        Variable1 = 0;  // Set Variable1 to 0
+        number = 0;  // Set number to 0
     }
 
-    // Convert Variable1 into a string, so we can change the text alignment to the right:
+    // Convert number into a string, so we can change the text alignment to the right:
     // It can be also used to add or remove decimal numbers.
     char string[10];  // Create a character array of 10 characters
     // Convert float to a string:
-    dtostrf(Variable1, 3, 0, string);  // (<variable>,<amount of digits we are going to use>,<amount of decimal digits>,<string name>)
+    dtostrf(number, 3, 0, string);  // (<variable>,<amount of digits we are going to use>,<amount of decimal digits>,<string name>)
 
     display.clearDisplay();  // Clear the display so we can refresh
-
     display.setFont(&FreeMono9pt7b);  // Set a custom font
     display.setTextSize(0);  // Set text size. We are using a custom font so you should always use the text size of 0
 
@@ -138,7 +139,7 @@ void loop()  // Start of loop
 
     // Print variable with left alignment:
     display.setCursor(83, 20);  // (x,y)
-    display.println(Variable1);  // Text or value to print
+    display.println(number);  // Text or value to print
 
     // Draw rounded rectangle:
     display.drawRoundRect(79, 37, 49, 27, 8, WHITE);  // Draw rounded rectangle (x,y,width,height,radius,color)
@@ -150,4 +151,4 @@ void loop()  // Start of loop
 
     display.display();  // Print everything we set previously
 
-}  // End of loop
+}*/
